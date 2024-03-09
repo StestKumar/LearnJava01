cf9f6350eae42cae7a276d447a37fab78497d0ac
Prashant Kumar <softwaretesterKumar@gmail.com>
2024-03-09 13:00:03 +0000

    Integer r changed

--- src/RadiusofCircle.java
+++ src/RadiusofCircle.java
@@ -8,7 +8,7 @@ public class RadiusofCircle
 
     public static void main (String[]args)
     {
-     int r = 2; // declaration and initialisation
+     int r = 5; // declaration and initialisation
      double pie = 3.14, area = pie * (r * r);
 
             System.out.println("The area of the circle: "+area);
